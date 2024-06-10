c7ca32dc20ee22a7cd213b7b69eb2da49ee02b41
Nikita <niknakels@gmail.com>
2024-06-10 12:50:46 +1100

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Никита'
+print('Name: ' + name)
+age = 32
+print('Age: ' + str(age))
+new_age = '33'
+print('New age: ' + new_age)
+is_student = True
+print('Is Student: ' + str(is_student))
