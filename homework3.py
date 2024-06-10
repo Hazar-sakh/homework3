e1170c319c92eb7f5608614cf642ebbe0c24880e
Nikita <niknakels@gmail.com>
2024-06-10 14:23:43 +1100

    homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,7 @@ name = 'Никита'
 print('Name: ' + name)
 age = 32
 print('Age: ' + str(age))
-new_age = '33'
-print('New age: ' + new_age)
+age = age + 1
+print('New age: ' + str(age))
 is_student = True
 print('Is Student: ' + str(is_student))
