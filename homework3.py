be497e901161a7c05e954b0187670e52610f202a
Nikita <niknakels@gmail.com>
2024-06-12 14:39:56 +1100

    homework3.py

--- homework3.py
+++ homework3.py
@@ -1,8 +1,8 @@
 name = 'Никита'
 print('Name: ' + name)
 age = 32
-print('Age: ' + str(age))
-age = age + 1
+print('Age: ' + str(age)) # выводим целое число (int) как строку (str)
+age = age + 1 # вносим изменение в ранее созданную переменную
 print('New age: ' + str(age))
-is_student = True
-print('Is Student: ' + str(is_student))
+is_student = True # переменная с булевым значением
+print('Is Student: ' + str(is_student)) # выводим булево как строку через str
